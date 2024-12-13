ef6deead9cce041fdce5f1b2bf285b2dbc20f425
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-13 12:02:43 +0530

    Add files via upload

---
+++ ls2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+    public class ls2
+    {
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            HashSet<String> a=new HashSet<>();
+            System.out.println(a);
+            System.out.println(a);
+            Iterator itr=a.iterator();
+            while (itr.hasNext())
+            {
+                System.out.println(itr.next()+" ");
+            }
+
+
+        }
+    }
