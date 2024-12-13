c44f478db920b2c5e438d146f6807f4543842bb6
N mohammad shaaz <mohammadshaaz457@gmail.com>
2024-12-13 12:04:32 +0530

    Add files via upload

---
+++ ls1.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+
+public class ls1 {
+   public ls1() {
+   }
+
+   public static void main(String[] var0) {
+      LinkedList var1 = new LinkedList();
+      var1.add("hello");
+      var1.add("shaaz");
+      System.out.println(var1);
+      System.out.println(var1);
+   }
+}
